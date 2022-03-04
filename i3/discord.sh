acf6437e80465fdd3c48258ab00833c841970789
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2022-03-04 13:32:26 +0100

    Add more Discord things

--- i3/discord.sh
+++ i3/discord.sh
@@ -1,162 +1,239 @@
 #! /bin/bash
 set -eu -o pipefail
+# This is a script meant to interact with an instace of Firefox which runs the
+# web version of Discord and do some things Discord is not able to do on its
+# own. The browser has 'tridactly' (which is used to define the keybinds
+# pressed; they are bind to the JS shown in the comments), 'Window Titler' (set
+# “Messenger”) and 'Tab ReTitle' (to ensure that the Discord tab always has the
+# same name)
 
+FOCUS="false"
 case "$1" in
     invisible|offline)
-        REL_LOC=-125
+        # if(document.getElementById("status-picker-idle") == null)
+        #   document.querySelector("[aria-label=\"Set Status\"]").click();
+        # document.getElementById("status-picker-idle").click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsi"
         ;;
     dnd|do-not-disturb)
-        REL_LOC=-190
+        # if(document.getElementById("status-picker-idle") == null)
+        #   document.querySelector("[aria-label=\"Set Status\"]").click();
+        # document.getElementById("status-picker-dnd").click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsn"
         ;;
     idle|away)
-        REL_LOC=-245
+        # if(document.getElementById("status-picker-idle") == null)
+        #   document.querySelector("[aria-label=\"Set Status\"]").click();
+        # document.getElementById("status-picker-idle").click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsa"
         ;;
     online)
-        REL_LOC=-290
+        # if(document.getElementById("status-picker-idle") == null)
+        #   document.querySelector("[aria-label=\"Set Status\"]").click();
+        # document.getElementById("status-picker-online").click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gso"
         ;;
     mute)
-        REL_LOC_Y=25
-        REL_LOC_X=225
+        # qs = document.querySelector("[aria-label=\"Mute\"]");
+        # if (qs != null)
+        #   qs.click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsm"
         ;;
     deafen)
-        REL_LOC_Y=25
-        REL_LOC_X=255
+        # qs = document.querySelector("[aria-label=\"Deafen\"]");
+        # if (qs != null)
+        #   qs.click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsd"
         ;;
     leave|disconnect)
-        REL_LOC_Y=115
-        REL_LOC_X=290
+        # qs = document.querySelector("[aria-label=\"Disconnect\"]");
+        # if (qs != null)
+        #   qs.click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsl"
         ;;
-    accept|join)
-        REL_LOC_Y=230
-        REL_LOC_X=155
+    accept|join|join-voice|accept-voice)
+        # qs = document.querySelector("[aria-label=\"Join Call\"]");
+        # if (qs != null)
+        #   qs.click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsj"
+        ;;
+    accept-video|join-video)
+        # qs = document.querySelector("[aria-label=\"Join Video Call\"]");
+        # if (qs != null)
+        #   qs.click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsJ"
         ;;
     reject|dismiss)
-        REL_LOC_Y=230
-        REL_LOC_X=80
+        # qs = document.querySelector("[aria-label=\"Dismiss\"]");
+        # if (qs != null)
+        #   qs.click();
+        # tri.excmds.focusinput("-l");
+        KEYS="gsr"
         ;;
-    update)
-        REL_LOC_Y=25
-        REL_LOC_X=110
+    call|voice-call)
+        # qs  = document.querySelector("[aria-label=\"Start Voice Call\"]");
+        # if (qs != null) {
+        #   qs.click();
+        #   tri.excmds.focusinput("-l");
+        # }
+        KEYS="gsc"
+        ;;
+    video|video-call|camera)
+        # qs  = document.querySelector("[aria-label=\"Start Video Call\"],[aria-label=\"Turn on Camera\"],[aria-label=\"Turn off Camera\"]");
+        # if (qs != null) {
+        #   qs.click();
+        #   tri.excmds.focusinput("-l");
+        # }
+        KEYS="gsv"
+        ;;
+    video-stop|video-call-stop|camera-stop)
+        # qs  = document.querySelector("[aria-label=\"Turn off Camera\"]");
+        # if (qs != null) {
+        #   qs.click();
+        #   tri.excmds.focusinput("-l");
+        # }
+        KEYS="gsV"
+        ;;
+    share|share-screen)
+        # qs = document.querySelector("[aria-label=\"Stop Streaming\"]");
+        # if (qs != null) {
+        #   qs.click();
+        #   tri.excmds.focusinput("-l");
+        # } else {
+        #   qs = document.querySelector("[aria-label=\"Share Your Screen\"]");
+        #   if (qs != null)
+        #     qs.click();
+        # }
+        KEYS="gss"
+        FOCUS="true"
+        ;;
+    stop-share|share-screen-stop)
+        # qs = document.querySelector("[aria-label=\"Stop Streaming\"]");
+        # if (qs != null) {
+        #   qs.click();
+        #   tri.excmds.focusinput("-l");
+        # }
+        KEYS="gsS"
         ;;
     go|go-away)
-        set -- idle
-        # shellcheck disable=SC1090
-        . "$0"
-        set -- mute
-        # shellcheck disable=SC1090
-        . "$0"
-        exit
+        # not at all far too complicated or anything
+        # only do something if there is no state and save state of status, mute and video
+
+        # if (typeof noxgrim_status === "undefined" && typeof noxgrim_was_muted === "undefined" && typeof noxgrim_was_video === "undefined") {
+        #     switch (document.querySelector("[aria-label=\"Set Status\"]>[aria-label]").getAttribute("aria-label").split(", ").pop()) {
+        #         case "Online":
+        #             noxgrim_status = "online";
+        #             break;
+        #         case "Idle":
+        #             noxgrim_status = "idle";
+        #             break;
+        #         case "Do Not Disturb":
+        #             noxgrim_status = "dnd";
+        #             break;
+        #         case "Invisible":
+        #             noxgrim_status = "invisible";
+        #             break;
+        #     }
+        #     if (noxgrim_status !== "invisible") {
+        #         if (document.getElementById("status-picker-idle") == null)
+        #             document.querySelector("[aria-label=\"Set Status\"]").click();
+        #         document.getElementById("status-picker-idle").click();
+        #     }
+        #     noxgrim_was_muted = document.querySelector("[aria-label=\"Mute\"] path[class^=\"strikethrough\"]") != null;
+        #     if (!noxgrim_was_muted)
+        #         document.querySelector("[aria-label=\"Mute\"]").click();
+        #     noxgrim_was_video = document.querySelector("[aria-label=\"Turn off Camera\"]");
+        #     if (noxgrim_was_video != null)
+        #         noxgrim_was_video.click();
+        #     noxgrim_was_video = noxgrim_was_video != null;
+        # }
+        # tri.excmds.focusinput("-l");
+        KEYS="gsg"
         ;;
     come|come-back)
-        set -- online
-        # shellcheck disable=SC1090
-        . "$0"
-        set -- mute
-        # shellcheck disable=SC1090
-        . "$0"
-        exit
+        # not at all far too complicated or anything
+        # restore previously saved status, mute and video
+
+        # if (typeof noxgrim_status !== "undefined" && typeof noxgrim_was_video !== "undefined" && typeof noxgrim_was_muted !== "undefined") {
+        #     if (noxgrim_status !== "invisible") {
+        #         if (document.getElementById("status-picker-idle") == null)
+        #             document.querySelector("[aria-label=\"Set Status\"]").click();
+        #         document.getElementById("status-picker-" + noxgrim_status).click();
+        #     }
+        #     noxgrim_muted = document.querySelector("[aria-label=\"Mute\"] path[class^=\"strikethrough\"]") != null;
+        #     if (noxgrim_muted && !noxgrim_was_muted)
+        #         document.querySelector("[aria-label=\"Mute\"]").click();
+        #     noxgrim_video = document.querySelector("[aria-label=\"Turn on Camera\"]");
+        #     if (noxgrim_video != null && noxgrim_was_video)
+        #         noxgrim_video.click();
+        # }
+        # noxgrim_was_muted = noxgrim_muted = noxgrim_was_video = noxgrim_video = noxgrim_status = undefined;
+        # tri.excmds.focusinput("-l");
+        KEYS="gsb"
         ;;
-    none);;
+    none)
+        exit 0
+        ;;
     usage);&
     *)
         echo 'Unknown command: '"${1:-}" >&2
         echo 'Use: invisible|offline,dnd|do-not-disturb,idle|away,online' >&2
         echo '     mute,deafen' >&2
-        echo '     accept|join,reject|dismiss,leave|disconnect' >&2
-        echo '     update' >&2
-        echo '     go|go-away,come|come-back' >&2
+        echo '     accept|accept-voice|join|join-voice,reject|dismiss,leave|disconnect' >&2
+        echo '     accept-video|join-video' >&2
+        echo '     video|video-call|camera' >&2
+        echo '     video-stop|video-call-stop|camera-stop' >&2
+        echo '     share|share-screen' >&2
+        echo '     stop-share|share-screen-stop' >&2
+        echo '     go|go-away,back|come-back' >&2
         exit 1
         ;;
 esac
 
-DISCORD_WINDOWS="$(xdotool search --class "discord")"
+DISCORD_WINDOWS="$(xdotool search --class "noxgrim_messengers" | # the class can be set with FF's ‘--class’ option
+                   xargs  -I{} sh -c 'if xprop -id {} | grep -q ^WM_WINDOW_ROLE".*browser"; then echo {}; fi')"
+[ -z "$DISCORD_WINDOWS" ] && echo "Discord not found!" >&2 && exit 1
 for ID in $DISCORD_WINDOWS; do
-    if xprop -id "$ID" | grep -q '^WM_STATE(WM_STATE):'; then
-        eval "$(xdotool getwindowgeometry --shell "$ID" | sed 's/^/WIN_/')"
-        eval "$(xdotool getmouselocation  --shell | sed 's/^/MOUSE_/')"
+    if ! xprop -id "$ID" | grep '^WM_NAME' | grep -q '\[Messenger] Discord'; then
+        xdotool key  --window "$ID" --delay 50 Ctrl+l Tab Tab Tab Escape
+        sleep 0.050s
+        xdotool type --window "$ID" --delay 50 --args 1 bDiscord
+        sleep 0.050s
+        xdotool key  --window "$ID" Enter
+
         WIN_ACTIVE="$(xdotool getactivewindow)"
-        TAKE_FROM_SCRATCHPAD=false
-        WAS_IN_SCRATCHPAD=$(xprop -id "$ID" | grep -q '^\s*window state: Withdrawn' && echo true || echo false)
+        set +o pipefail
+        WAS_IN_SCRATCHPAD=$(xprop -id "$ID" | grep -qm 1 '^\s*window state: Withdrawn' && echo true || echo false)
+        set -o pipefail
         WAS_FULL_SCREEN="$(i3-msg -t get_tree | jq '..|select(.window? and .window_type? and .window_type and .window=='"$WIN_ACTIVE"')|.fullscreen_mode==1')"
-
-        if "${NOT_CALCED_OFF_SCREEN:-true}"; then
-            WIN_NE_OFFSCREEN=true
-            WIN_NW_OFFSCREEN=true
-            WIN_SE_OFFSCREEN=true
-            WIN_SW_OFFSCREEN=true
-            WIN_SE_X="$((WIN_X+WIN_WIDTH))"
-            WIN_SE_Y="$((WIN_Y+WIN_HEIGHT))"
-            CORNERS_OFFSCREEN=4
-            # Is the window offscreen (i.e. do we have to move it to be able to click it properly)?
-            while read -r XRANDR_LINE && [ $CORNERS_OFFSCREEN != 0 ]; do
-                # shellcheck disable=2001
-                eval "$(sed 's/\([0-9]*\)[^x]*x\([0-9]*\)[^+]*+\([0-9]*\)+\([0-9]*\)/MON_WIDTH="\1";MON_HEIGHT="\2";MON_X="\3";MON_Y="\4";/' <<< "$XRANDR_LINE")"
-                if "$WIN_NE_OFFSCREEN" && ((WIN_X>=MON_X&&WIN_X<(MON_X+MON_WIDTH)&&WIN_Y>=MON_Y&&WIN_Y<(MON_Y+MON_HEIGHT))); then
-                    WIN_NE_OFFSCREEN=false
-                    ((CORNERS_OFFSCREEN--))
-                fi
-                if "$WIN_NW_OFFSCREEN" && ((WIN_SE_X>=MON_X&&WIN_SE_X<(MON_X+MON_WIDTH)&&WIN_Y>=MON_Y&&WIN_Y<(MON_Y+MON_HEIGHT))); then
-                    WIN_NW_OFFSCREEN=false
-                    ((CORNERS_OFFSCREEN--))
-                fi
-                if "$WIN_SE_OFFSCREEN" && ((WIN_X>=MON_X&&WIN_X<(MON_X+MON_WIDTH)&&WIN_SE_Y>=MON_Y&&WIN_SE_Y<(MON_Y+MON_HEIGHT))); then
-                    WIN_SE_OFFSCREEN=false
-                    ((CORNERS_OFFSCREEN--))
-                fi
-                if "$WIN_SW_OFFSCREEN" && ((WIN_SE_X>=MON_X&&WIN_SE_X<(MON_X+MON_WIDTH)&&WIN_SE_Y>=MON_Y&&WIN_SE_Y<(MON_Y+MON_HEIGHT))); then
-                    WIN_SW_OFFSCREEN=false
-                    ((CORNERS_OFFSCREEN--))
-                fi
-            done < <(xrandr --listactivemonitors | tail +2 | cut -d':' -f2- | cut -d' ' -f3)
-            NOT_CALCED_OFF_SCREEN=false
+        xdotool windowactivate "$ID"
+        sleep 0.05s
+        "$WAS_IN_SCRATCHPAD" && i3-msg "[id=$ID]" scratchpad show > /dev/null || true
+        xdotool windowactivate "$WIN_ACTIVE"
+        if "$WAS_FULL_SCREEN" [ "$WIN_ACTIVE" != "$ID" ]; then
+            i3-msg "[id=$WIN_ACTIVE]" fullscreen > /dev/null || true
         fi
-        if [ "$CORNERS_OFFSCREEN" -gt 0 ]; then
-            if [ "$WAS_IN_SCRATCHPAD" = true ]; then
-                TAKE_FROM_SCRATCHPAD=true
-                xdotool windowactivate --sync "$ID"
-            fi
-            xdotool windowmove --sync "$ID"   "$MON_X" "$MON_Y"
+        if ! xprop -id "$ID" | grep '^WM_NAME' | grep -q '\[Messenger] Discord'; then
+            xdotool key  --window "$ID" Escape
+            echo "Discord not found!" >&2 && exit 1
+        else
+            echo "$ID $KEYS (active:$WIN_ACTIVE discord-scratchpad:$WAS_IN_SCRATCHPAD active-fullscreen:$WAS_FULL_SCREEN)"
         fi
-
-        case "$1" in
-            invisible|offline|dnd|do-not-disturb|idle|away|online)
-                xdotool\
-                    mousemove          --sync --clearmodifiers --window "$ID" -- 95 "$((WIN_HEIGHT-25))"\
-                    click                     --clearmodifiers --window "$ID" 1\
-                    mousemove_relative --sync --clearmodifiers                -- 0 "$REL_LOC"
-                sleep 0.1
-                xdotool\
-                    click                     --clearmodifiers --window "$ID" 1
-                ;;
-            mute|deafen|leave|disconnect)
-                xdotool\
-                    mousemove          --sync --clearmodifiers --window "$ID" -- "$REL_LOC_X" "$((WIN_HEIGHT-REL_LOC_Y))"\
-                    click                     --clearmodifiers --window "$ID" 1
-                ;;
-            accept|join|reject|dismiss)
-                xdotool\
-                    mousemove          --sync --clearmodifiers --window "$ID" -- "$REL_LOC_X" "$REL_LOC_Y"\
-                    click                     --clearmodifiers --window "$ID" 1
-                ;;
-            update)
-                xdotool\
-                    mousemove          --sync --clearmodifiers --window "$ID" -- "$((WIN_WIDTH-REL_LOC_X))" "$REL_LOC_Y"\
-                    click                     --clearmodifiers --window "$ID" 1
-                ;;
-        esac
-
-        xdotool\
-            mousemove "$MOUSE_X" "$MOUSE_Y"
-        if [ "$WIN_X" -lt 0 ] || [ "$WIN_Y" -lt 0 ]; then
-            xdotool windowmove --sync "$ID" "$WIN_X" "$WIN_Y"
-        fi
-        if [ "$TAKE_FROM_SCRATCHPAD" = true ]; then
-            i3-msg  '[class="discord"]' scratchpad show > /dev/null
-        fi
-        xdotool windowactivate --sync "$WIN_ACTIVE"
-        if [ "$WAS_FULL_SCREEN" = true ] && [ "$(i3-msg -t get_tree | jq 'recurse(.nodes[])|select(.window_type and .window=='"$WIN_ACTIVE"')|.fullscreen_mode==1')" = false ]; then
-            i3-msg  '[id='"$WIN_ACTIVE"']' fullscreen
-        fi
-        break
+    else
+        echo "$ID $KEYS"
     fi
+    xdotool key  --window "$ID" --delay 50 Ctrl+l Tab Tab Tab Escape
+    sleep 0.05s
+    xdotool type --window "$ID" --delay 50 --args 1 "$KEYS"
+    "$FOCUS" && xdotool windowactivate "$ID" || true
 done
+[ -z "$DISCORD_WINDOWS" ] && echo "Discord not found!" >&2 && exit 1
