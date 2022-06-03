68fbd6fc261e860ae732585201a0317cc9f04ed5
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2022-06-03 15:55:41 +0200

    Use hardcoded redshift config

--- i3/environment.sh
+++ i3/environment.sh
@@ -4,6 +4,6 @@ numlockx on
 pgrep -u "$USER" dunst        || dunst &
 pgrep -u "$USER" keynav       || keynav &
 pgrep -u "$USER" picom        || picom -CGbi 1 &
-pgrep -u "$USER" redshift-gtk || redshift-gtk -l geoclue2 &
+pgrep -u "$USER" redshift-gtk || redshift-gtk -l geoclue2 -t 6500K:2500K -b 1.0:0.7 &
 pgrep -u "$USER" xss-lock     || xss-lock -l -- "$HOME/.i3/lock.sh" &
 pgrep -u "$USER" nm-applet    || nm-applet &
