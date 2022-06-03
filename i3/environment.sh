c559af4f9e143515e5df1c15db6dc25ff9ee871e
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2022-06-03 15:48:07 +0200

    Add keynav config

--- i3/environment.sh
+++ i3/environment.sh
@@ -2,6 +2,7 @@
 source "$HOME/.device_specific/wallpaper_command.sh"
 numlockx on
 pgrep -u "$USER" dunst        || dunst &
+pgrep -u "$USER" keynav       || keynav &
 pgrep -u "$USER" picom        || picom -CGbi 1 &
 pgrep -u "$USER" redshift-gtk || redshift-gtk -l geoclue2 &
 pgrep -u "$USER" xss-lock     || xss-lock -l -- "$HOME/.i3/lock.sh" &
