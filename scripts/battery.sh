9612e77d98c61fbf35b0c63d1929010e95cfb44b
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2023-03-25 22:43:09 +0100

    Version battery service
    The udev rule triggers the service again so that the notification can be
    cleared (you know, pressing <C-space> is really annoying…)

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-source /home/noxgrim/.i3/notify.sh
-acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
-    read -r status capacity
+source /root/notify.sh
+acpi -b | sed 's/[^:]*:\s*\([^,]*\),\s*\([^%]*\)%.*/\1,\2/' | {
+    IFS=, read -r status capacity
 
     if [ "$status" = Discharging ]; then
         if [ "$capacity" -lt 5 ]; then
             logger "Critical battery threshold"
-            notify -u critical "'Critical battery threshold'" "Hibernating..."
+            notify -u critical -a 'noxgrim:power' -h "int:value:$capacity" "Low battery level: " % -t 1
+            notify -u critical "Critical battery threshold" "Hibernating..."
             sleep 5
             systemctl hibernate -i
         elif [ "$capacity" -le 15 ]; then
-            notify -u critical -a 'noxgrim:volume' -h "int:value:$capacity" "'Low battery level: '" %
+            notify -u critical -a 'noxgrim:power' -h "int:value:$capacity" "Low battery level: " %
         fi
+    elif [ "$capacity" -le 17 ]; then
+        notify -u critical -a 'noxgrim:power' -h "int:value:$capacity" "Low battery level: " % -t 1
     fi
 }
--- setup.sh
+++ setup.sh
@@ -10,3 +10,15 @@ done
 ln -sfr .git-precommit '.git/hooks/pre-commit' # The censorer
 
 [ -d "$HOME/.mpd/playlists" ] || mkdir -p "$HOME/.mpd/playlists"
+
+# Do we have a battery?
+if [ -n "$(find /sys/class/power_supply -iname 'BAT*' -print -quit)"  ]; then
+  sudo cp scripts/{battery,notify}.sh /root
+  sudo cp systemd/system/battery.{service,timer} /etc/systemd/system
+  sudo pacman -Syu acpid
+  sudo systemctl daemon-reload
+  sudo systemctl enable battery.timer
+  sudo systemctl start battery.timer
+  [ -d '/etc/udev/rules.d' ] || sudo mkdir -p '/etc/udev/rules.d'
+  sudo cp -a {,/etc/}udev/rules.d/61-charging.rules
+fi
