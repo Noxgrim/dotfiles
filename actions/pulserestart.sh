414dee7ca49d2975a47a5bb2eed708225c8b0523
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2023-09-10 16:45:33 +0200

    Fix syntax in pulserestart action

--- actions/pulserestart.sh
+++ actions/pulserestart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-while !killall -9 pulseaudio; do sleep 1; done
+while ! killall -9 pulseaudio; do sleep 1; done
 sleep 2
 systemctl --user restart pulseaudio.service
 systemctl --user restart mpd.service
