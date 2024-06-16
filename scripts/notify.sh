118f708075a02bc3acbcc1a74a187a3ad59566b1
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2024-06-16 11:49:28 +0200

    Make notify.sh export all variables and return after the first match
    + Let execute export the whole environment
    + Let execute return after the first matching client
    + Add ksmserver for KDE support
    + Change notify to be alias for `execute notify-send $@`
      Why this script is still called notify beats me.
      Legacy I guess.

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -1,34 +1,32 @@
 # https://bbs.archlinux.org/viewtopic.php?pid=902722#p902722
-notify() {
-    set -- "${@//"'"/"'\\''"}"; set -- "${@/#/"'"}"; set -- "${@/%/"'"}"
-    for pid in $(pgrep -x 'i3' -u "$(id -u "$USER")"); do
-        eval "$(grep -z ^USER /proc/"$pid"/environ | tr '\000' '\n')"
-        eval export "$(grep -z ^DISPLAY /proc/"$pid"/environ | tr '\000' '\n')"
-        eval export "$(grep -z ^DBUS_SESSION_BUS_ADDRESS /proc/"$pid"/environ | tr '\000' '\n')"
-        eval export "$(grep -z ^SCRIPT_ROOT /proc/"$pid"/environ | tr '\000' '\n')"
-
-        export IFS=' '
-        if [ "$(whoami)" = "$USER" ]; then
-            sh -c "notify-send  $*"
-        else
-            su "$USER" -c "notify-send  $*"
-        fi
-        echo "$1 $2"
-    done
-}
 execute() {
     set -- "${@//"'"/"'\\''"}"; set -- "${@/#/"'"}"; set -- "${@/%/"'"}"
-    for pid in $(pgrep -x 'i3'  -u "$(id -u "$USER")"); do
-        eval "$(grep -z ^USER /proc/"$pid"/environ | tr '\000' '\n')"
-        eval export "$(grep -z ^DISPLAY /proc/"$pid"/environ | tr '\000' '\n')"
-        eval export "$(grep -z ^DBUS_SESSION_BUS_ADDRESS /proc/"$pid"/environ | tr '\000' '\n')"
-        eval export "$(grep -z ^SCRIPT_ROOT /proc/"$pid"/environ | tr '\000' '\n')"
+    local -a TARGETS
+    if pgrep -x i3 >/dev/null; then
+        TARGETS+=(i3)
+    fi
+    if pgrep -x ksmserver >/dev/null; then
+        TARGETS+=(ksmserver)
+    fi
+    if [ "${#TARGETS[@]}" = 0 ]; then
+        TARGETS+=(zsh)
+    fi
+    (
+    for TARGET in "${TARGETS[@]}"; do
+        for pid in $(pgrep -x "$TARGET" -u "$(id -u "$USER")"); do
+            eval export "$(sed -z "s/'/'\\\\''/g"';s/=\(.*\)$/='"'\\1'/" /proc/"$pid"/environ | tr '\000' ' ')"
 
-        export IFS=' '
-        if [ "$(whoami)" = "$USER" ]; then
-            sh -c "$*"
-        else
-            su "$USER" -c "$*"
-        fi
+            export IFS=' '
+            if [ "$(whoami)" = "$USER" ]; then
+                sh -c "$*"
+            else
+                su "$USER" -c "$*"
+            fi
+            return "$?"
+        done
     done
+    )
+}
+notify() {
+    execute 'notify-send' "${@}"
 }
