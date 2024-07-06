8f9ae1830bd37438d188a9f0443fcde12220a6cc
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2024-07-06 21:22:26 +0200

    Add (un)mounting function, desktop files and init repositories
    + Add function to mount and unount which uses zenity for feedback
    + Add desktop file(s) to start the backup from a user login session
    + Add instanced service for calling the backup script with a given
      argument
    + Add password reading function
    * Assume UID 1000 by default for USER_TARGET

--- backup/backup.sh
+++ backup/backup.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 
 [ "$(id -u)" != 0 ] && echo 'Must run as root!' >&2 && exit 1
-[ -z "$USER_TARGET" ] && echo "Must define \$USER_TARGET" >&2 && exit 1
+USER_TARGET="${USER_TARGET-"$(id -nu 1000)"}"
 
 special_vars() {
     clean() {
@@ -47,6 +47,7 @@ readonly CACHE="${XDG_CACHE_DIR-"$THISDIR/.cache"}/backup"
 readonly SUSPEND_F="/tmp/backup_suspend"
 readonly DISABLE_F="/tmp/backup_disable"
 readonly SKIPPED_F="$CACHE/skipped"
+readonly ARCHIVE_MOUNT_STATE_F="$CACHE/repo_mount_state"
 readonly SKIP_RANSOM_F="$CACHE/skip_next_ransom"
 readonly CONFIG_WORLD="$THISDIR/backup.conf.world"
 
@@ -341,11 +342,11 @@ get_password() {
 }
 
 mount_device() {
-    local MNT_SOURCE
+    local MNT_SOURCE MOUNT_MESSAGE_HEAD="${1-"Backing up..."}"
     # mount backup drive
     PASSPHRASE="$(get_password)" || exit 1
 
-    $NOTIFY_START && send_message low "Backing up..." "<u>Keep device connected!</u>"
+    $NOTIFY_START && send_message low "$MOUNT_MESSAGE_HEAD" "<u>Keep device connected!</u>"
     TIME="$(date +%s)"
 
     # shellcheck disable=2174
@@ -404,7 +405,8 @@ do_backup() {
     local OUT EXIT CONTINUE MESSAGE_HEAD MESSAGE_BODY MESSAGE_URGENCY TIME_FMT
     OUT="$(mktemp)"
     chmod 600 "$OUT"
-    set +e
+    borg list -a "*$BACKUP_KEEP_ARCHIVE_PATTERN" --first 1 3<<<"$PASSPHRASE" | grep -q . || \
+        BACKUP_ARCHIVE_NAME="$BACKUP_ARCHIVE_NAME_FIRST_PREFIX$BACKUP_ARCHIVE_NAME"
     borg create --verbose --stats --compression "$BACKUP_COMPRESSION" --list --filter AME \
         --show-rc ::"$BACKUP_ARCHIVE_NAME" \
         --patterns-from <(
@@ -423,9 +425,8 @@ do_backup() {
         done
         echo "$BACKUP_PATTERN_POST_EXCLUDE_FILE"
     ) "${BACKUP_ARGS[@]}" 3<<< "$PASSPHRASE" \
-        &> "$OUT" # avoid log spam while log is updated
+        &> "$OUT" && true # avoid log spam while log is updated
     EXIT="$?"
-    set -e
 
     cat "$OUT"
     rm "$OUT"
@@ -445,7 +446,7 @@ do_backup() {
             MESSAGE_BODY="<u>There were warinings!</u>\n"
             ;;
         2) # Error
-            CONTINUE=false # we con't want to interact more with the repository
+            CONTINUE=false # we don't want to interact more with the repository
             MESSAGE_URGENCY=critical
             MESSAGE_HEAD="Backup may be incomplete!"
             MESSAGE_BODY="<u>An backup error occurred!</u>\n"
@@ -493,10 +494,13 @@ install_packages() {
     # commands and packages on arch
     PACKAGE_OPTIONS['borg']='borg'
     PACKAGE_OPTIONS['cryptsetup']='cryptsetup'
+    $INSTALL_DESKTOP_FILES && PACKAGE_OPTIONS['desktop-file-install']='desktop-file-util'
     PACKAGE_OPTIONS['expect']='expect'
     PACKAGE_OPTIONS['gpg']='gnupg'
     PACKAGE_OPTIONS['locate']='plocate'
+    PACKAGE_OPTIONS['lsof']='lsof'
     PACKAGE_OPTIONS['notify-send']='libnotify'
+    $INSTALL_DESKTOP_FILES && PACKAGE_OPTIONS['pkexec']='polkit'
     PACKAGE_OPTIONS['ssh']='openssh'
     PACKAGE_OPTIONS['xdg-user-dir']='xdg-user-dirs'
     PACKAGE_OPTIONS['zenity']='zenity'
@@ -522,12 +526,13 @@ install_packages() {
     fi
 }
 
-setup() {
-    local BUSER_HOME CONFIG="${1?"Please sepcify the config file!"}"
+setup_cmd() {
+    local BUSER_HOME CONFIG="${1?"Please sepcify the config file!"}" USER_TARGET_ID
     BUSER_HOME="$(grep HOME /etc/default/useradd | cut -d= -f2-)"
+    USER_TARGET_ID="$(id -u "$USER_TARGET")"
     BUSER_HOME="${BUSER_HOME-/home}/.backup"
     CONFIG="$(readlink -f -- "$CONFIG")"
-    readonly BUSER_HOME CONFIG
+    readonly BUSER_HOME CONFIG USER_TARGET_ID
     readonly UDEV_RULE='/etc/udev/rules.d/61-backup-root-mount.rules'
     # shellcheck disable=1090
     source "$CONFIG"
@@ -541,7 +546,7 @@ setup() {
     [ ! -e "$DEVPATH" ] && echo "Please connect specified backup device! (${DEVICEUUID-"$DEVICEPATH"})" >&2 && exit 1
     cp "$THISDIR/../scripts/notify.sh" /root
     # shellcheck disable=2016
-    sed -i 's,\$(id -u "\$USER"),1000,g' /root/notify.sh # send messages to user
+    sed -i 's,\$(id -u "\$USER"),'"$USER_TARGET_ID"',g' /root/notify.sh # send messages to user
     # shellcheck disable=1091
     source /root/notify.sh
 
@@ -704,23 +709,81 @@ EOF
 
     # systemd units
     echo "Setting up and stating systemd units…"
-    cp "$THISDIR/backup."{service,timer} /etc/systemd/system
+    cp "$THISDIR/backup."{service,timer} "$THISDIR/backup@.service" /etc/systemd/system
     systemctl disable backup.service backup.timer || true
     sed '/OnCalendar/s/\(OnCalendar\)\s=\s*.*/\1='"$BACKUP_FREQUENCY/" -i '/etc/systemd/system/backup.timer'
+    sed '/^ExecStart/s/\(id -nu \)1000)/\1'"$USER_TARGET_ID)/" -i  '/etc/systemd/system/backup.service' '/etc/systemd/system/backup@.service'
     systemctl daemon-reload
     systemctl enable backup.timer
     systemctl start backup.timer
+
+    if $INSTALL_DESKTOP_FILES; then
+        local TEMDIR ACTION FILE EXEC
+        local -A COMMENTS NAMES
+        NAMES['backup']='Run backup'
+        NAMES['mount']='Mount backup'
+        NAMES['umount']='Umount backup'
+        COMMENTS['backup']='Run a complete backup now'
+        COMMENTS['mount']='Mount a backup archive or repository to browse it'
+        COMMENTS['umount']='Unmount a previously mounted backup archive or repository'
+        TEMDIR="$(mktemp -d '/tmp/desktop_files.XXXXXXXXXXX.d')"
+        chmod 700 "$TEMDIR"
+        for ACTION in backup mount umount; do
+            FILE=backup
+            $INSTALL_DESKTOP_FILES_FILES_FOR_ACTIONS && FILE="$ACTION"
+            FILE="$TEMDIR/$FILE.desktop"
+
+            if [ "$ACTION" = 'backup' ] || $INSTALL_DESKTOP_FILES_FILES_FOR_ACTIONS; then
+                cat >> "$FILE" << EOF
+[Desktop Entry]
+Name=${NAMES["$ACTION"]}
+Version=1.0
+Comment=${COMMENTS["$ACTION"]}
+GenericName=Backup
+Keywords=System;Backup;Restore;Time Machine;File History
+Type=Application
+StartupNotify=true
+Categories=System;Utility;Archiving;
+EOF
+            else
+                cat >> "$FILE" << EOF
+
+[Desktop Action $ACTION]
+Name=${COMMENTS["$ACTION"]}
+EOF
+            fi
+            if [ "$ACTION" = 'backup' ] && ! $INSTALL_DESKTOP_FILES_FILES_FOR_ACTIONS; then
+                cat >> "$FILE" << EOF
+Actions=mount;umount;
+EOF
+            fi
+            if [ "$ACTION" = 'backup' ]; then
+                EXEC='/bin/sh -c "pkexec systemctl start backup.service &>/dev/null"'
+            else
+                EXEC='/bin/sh -c "pkexec systemctl start backup@'$ACTION'.service &>/dev/null"'
+            fi
+            cat >> "$FILE" << EOF
+Exec=$EXEC
+EOF
+        done
+        desktop-file-install --vendor backup "$TEMDIR"/*.desktop
+        rm -rf "$TEMDIR"
+    fi
 
     write_config_world
 
     echo 'Done!'
 }
 
-backup() {
+init() {
     # shellcheck disable=1091
     source /root/notify.sh
     # shellcheck disable=1091
     source "$THISDIR/backup.conf"
+}
+
+backup_cmd() {
+    init
     write_config_world
     check_other_devices
     check_should_run
@@ -733,24 +796,218 @@ backup() {
     do_backup
 }
 
+password_cmd() {
+    init
+    get_password
+}
+
+get_archive_mountloc() {
+    if [ -z "$ARCHIVE_MOUNTLOC" ]; then
+        ! $ARCHIVE_MOUNT_NON_ROOT_READ  && send_message critical 'Backup config error!' \
+            'Cannot determine archive mount location.'$'\n''Not readable for non root users and ARCHIVE_MOUNTLOC empty!' && exit 1
+        ARCHIVE_MOUNTLOC="/run/media/$USER_TARGET/backup_archive"
+    fi
+}
+
+check_device_busy() {
+    if lsof +f -F cn -- "$1" 2>/dev/null | grep -q .; then
+        lsof +f -F c0n0 -- "$1" 2>/dev/null | \
+            sed ':a /\x00$/{N;s/\x00\n/\x00/;b a;}' | sed -z '$d;s/^.//' | \
+            execute xargs -r0 zenity --list --column PID --column Command --column File \
+                --text "There are still files on '$1' open" --title "Cannot umount" \
+                --cancel-label "" &>/dev/null || true
+        exit 1
+    fi
+}
+
+umount_cmd() {
+    init
+    local FORCE="${1?"Please specify an argument which may be empty or -f"}" ARCHIVE_MOUNT_STATE_F_LOC='' EC=0
+    [ -e "$ARCHIVE_MOUNT_STATE_F" ] && ARCHIVE_MOUNT_STATE_F_LOC="$(readlink -f "$ARCHIVE_MOUNT_STATE_F")"
+    readonly FORCE
+    get_archive_mountloc
+
+    if [ -e "$ARCHIVE_MOUNT_STATE_F" ]; then
+        # shellcheck disable=1090
+        source "$ARCHIVE_MOUNT_STATE_F"
+    elif [ "$FORCE" = '-f' ]; then
+        [ -h "$ARCHIVE_MOUNT_STATE_F" ] && rm "$ARCHIVE_MOUNT_STATE_F"
+        [ -e '/dev/mapper/backup' ] && DO_CLOSE=true || DO_CLOSE=false
+        findmnt --mountpoint "$MOUNTLOC" &>/dev/null && DO_UMOUNT=true || DO_UMOUNT=false
+        findmnt --mountpoint "$ARCHIVE_MOUNTLOC" &>/dev/null && USED_ARCHIVE_MOUNTLOC="$ARCHIVE_MOUNTLOC"
+    else
+        [ -h "$ARCHIVE_MOUNT_STATE_F" ] && rm "$ARCHIVE_MOUNT_STATE_F"
+        send_message critical 'Cannot unmount!' 'No priour mount detected' && exit 1
+    fi
+
+    if [ -n "${USED_ARCHIVE_MOUNTLOC-}" ] && findmnt --mountpoint "$USED_ARCHIVE_MOUNTLOC" &>/dev/null; then
+        check_device_busy "$USED_ARCHIVE_MOUNTLOC"
+        borg umount "$USED_ARCHIVE_MOUNTLOC" && true
+        EC="$?"
+        if [ $EC != 0 ]; then
+            send_message critical 'Cannot unmount!' "Encountered error code $EC while trying to unmount borg." && exit 1
+        fi
+        [ -e "$ARCHIVE_MOUNT_STATE_F" ] && sed -i '/USED_ARCHIVE_MOUNTLOC/d' "$ARCHIVE_MOUNT_STATE_F_LOC"
+        if [[ "$USED_ARCHIVE_MOUNTLOC" = /run/media/*/backup_archive ]]; then
+            rmdir "$USED_ARCHIVE_MOUNTLOC"
+            if ! find "${USED_ARCHIVE_MOUNTLOC%/*}" -mindepth 1 -maxdepth 1 | grep -q .; then
+                rmdir "${USED_ARCHIVE_MOUNTLOC%/*}";
+            fi
+        fi
+        sleep 2
+    fi
+    if $DO_UMOUNT; then
+        check_device_busy "$MOUNTLOC"
+        if ! umount "$MOUNTLOC"; then
+            sleep 10
+
+            umount "$MOUNTLOC" && true
+            EC="$?"
+            if [ $EC != 0 ]; then
+                send_message critical 'Cannot unmount!' "Encountered error code $EC while trying to unmount $MOUNTLOC." && exit 1
+            fi
+        fi
+        [ -e "$ARCHIVE_MOUNT_STATE_F" ] && sed -i '/DO_UMOUNT/d' "$ARCHIVE_MOUNT_STATE_F_LOC"
+    fi
+
+    if $DO_CLOSE; then
+        cryptsetup close backup && true
+        EC="$?"
+        if [ $EC != 0 ]; then
+            send_message critical 'Cannot unmount!' "Encountered error code $EC while trying to close encrypted volume." && exit 1
+        fi
+    fi
+    if $DO_UMOUNT || $DO_CLOSE; then
+        send_message low 'Unmounted backup device'
+    fi
+
+
+    [ -e "$ARCHIVE_MOUNT_STATE_F" ] && rm "$ARCHIVE_MOUNT_STATE_F_LOC"
+    [ -h "$ARCHIVE_MOUNT_STATE_F" ] && rm "$ARCHIVE_MOUNT_STATE_F"
+}
+
+
+mount_cmd() {
+    local ACTUAL_STATE_FILE WHAT="${1-}" TYPE=''
+    init
+    if [ -z "$WHAT" ]; then
+        $ARCHIVE_MOUNT_REPO && WHAT='-r' || WHAT='-a'
+    fi
+    get_archive_mountloc
+
+    if [ ! -e "/dev/disk/by-uuid/$DEVICEUUID" ]; then # check for existence
+        send_message low 'Cannot mount' 'Backup device not connected.'
+        exit 1;
+    fi
+
+    findmnt --mountpoint "$ARCHIVE_MOUNTLOC" &>/dev/null && send_message critical 'Cannot mount!' 'A borg archive is already mounted. Please umount it first.' && exit 1
+    if systemctl is-active -q backup.service || [ "$( pgrep -cf '(^|/)[b]org ' )" -gt 0 ] ; then # check for existence
+        send_message critical 'Cannot mount!' 'A backup or borg is already running.'$'\n''Please wait for its completion.' && exit 1
+    fi
+
+    mount_device 'Mounting backup device...'
+
+    ACTUAL_STATE_FILE="$(mktemp "/tmp/backup-XXXXXXXXXXX.state")"
+    chmod 600 "$ACTUAL_STATE_FILE"
+    ln -sfr "$ACTUAL_STATE_FILE" "$ARCHIVE_MOUNT_STATE_F"
+    cat > "$ARCHIVE_MOUNT_STATE_F" << EOF
+DO_CLOSE=$DO_CLOSE
+DO_UMOUNT=$DO_UMOUNT
+EOF
+
+    case "$WHAT" in
+        -r)
+            WHAT="$BORG_REPO"
+            TYPE='Repository'
+            ;;
+        -a)
+            local CHOICE
+            if borg list -a "$ARCHIVE_MOUNT_FILTER_PATTERN" 3<<<"$PASSPHRASE" | grep -q .; then
+                CHOICE="$(borg list --format '{time}{NUL}{name}{NUL}' \
+                    -a "$ARCHIVE_MOUNT_FILTER_PATTERN" 3<<<"$PASSPHRASE" |\
+                    sed -z '1!G;h;$!d' | execute xargs -r0 zenity --list \
+                    --title "Select archive to mount" --text "(Newest shown first)" --column Name --column Created || true)"
+                if [ -z "$CHOICE" ]; then
+                    umount_cmd ''
+                    exit 1
+                fi
+                WHAT="::$CHOICE"
+            else
+                send_message low 'Cannot mount.' 'No sutable archives found.'$'\n''Consider adjusting the filters.'
+                umount_cmd ''
+                exit 1
+            fi
+            TYPE='Archive'
+            ;;
+        *)
+            umount_cmd ''
+            echo 'What is your WHAT?!' >&2 && exit 1
+    esac
+
+    local -a ARGS=( mount "$WHAT" "$ARCHIVE_MOUNTLOC" )
+    $ARCHIVE_MOUNT_NON_ROOT_READ && ARGS+=( -o allow_other )
+
+    mkdir -p "$ARCHIVE_MOUNTLOC"
+    $ARCHIVE_MOUNT_NON_ROOT_READ && chown "$USER_TARGET:$USER_TARGET" "$ARCHIVE_MOUNTLOC"
+    if [ "$ARCHIVE_MOUNTLOC" = "/run/media/$USER_TARGET/backup_archive" ]; then
+        setfacl --set 'user::rwx,user:'"$USER_TARGET"':r-x,group::r-x,mask::r-x,other::---' \
+            "${ARCHIVE_MOUNTLOC%/*}"
+    else
+        chmod 700 "$ARCHIVE_MOUNTLOC"
+    fi
+
+    borg "${ARGS[@]}" 3<<< "$PASSPHRASE" && true
+    EC="$?"
+    if [ $EC != 0 ]; then
+        send_message critical 'Cannot mount!' "Encountered error code $EC while trying to borg mount."
+        unmount_cmd
+        exit 1
+    fi
+    cat >> "$ARCHIVE_MOUNT_STATE_F" << EOF
+USED_ARCHIVE_MOUNTLOC=$ARCHIVE_MOUNTLOC
+EOF
+    send_message low "$TYPE mounted" "Browse at ‘$ARCHIVE_MOUNTLOC’"
+}
 
 main() {
     case "${1-backup}" in
         setup)
-            setup "${2?"Please provide a config file to use."}"
+            setup_cmd "${2?"Please provide a config file to use."}"
             ;;
         backup)
-            backup
+            backup_cmd
+            ;;
+        password)
+            password_cmd
+            ;;
+        mount)
+            mount_cmd "${2-}"
+            ;;
+        umount|unmount)
+            umount_cmd "${2-}"
             ;;
         *)
             cat << 'EOF'
 Usage:
- start with `sudo env USER_TARGET="$USER" ~backup/backup.sh`
+ start with `~backup/backup.sh`
+ To define another user to target set evironment variable USER_TARGET
+ (default is user with id 1000).
+ `sudo env USER_TARGET="$USER" ~backup/backup.sh`
 Options:
     backup
         Perform a backup now
     setup CONFIG_FILE
-        Initialize and setup system for antomatic backups
+        Initialize and setup system for automatic backups
+    password
+        Print backup password to stdout, requesting it if necessary
+    mount [-a|-r]
+        Prompt for a borg archive and mount it
+        If -r is given, mount a whole repository
+        If -a is given, mount a specified archive
+        fi
+    umount [-f]
+        Unmount previously mounted borg archive
+        If -f is given, try to do that even if no prior mount was detected
 EOF
             ;;
     esac
