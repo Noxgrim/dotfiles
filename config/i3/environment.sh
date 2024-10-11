f9d70379a71b6a46cd2244f1a91ae210059f87d7
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2024-10-11 20:27:58 +0200

    Remove deprecated picom options, add transparency

--- config/i3/environment.sh
+++ config/i3/environment.sh
@@ -3,7 +3,7 @@ device wallpaper
 numlockx on
 pgrep -u "$USER" dunst        || dunst &
 pgrep -u "$USER" keynav       || keynav &
-pgrep -u "$USER" picom        || picom -CGbi 1 --shadow-exclude 'name ?= "cpt_frame_xcb_window" || name ?= "as_toolbar"' -- &
+pgrep -u "$USER" picom        || picom &
 pgrep -u "$USER" redshift-gtk || redshift-gtk -l geoclue2 -t 6500K:2500K -b 1.0:0.7 &
 pgrep -u "$USER" xss-lock     || xss-lock -l -- "$SCRIPT_ROOT/config/i3/lock.sh" & disown
 pgrep -u "$USER" nm-applet    || nm-applet &
