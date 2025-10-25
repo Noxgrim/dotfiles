63c05e24a121769a4775c30a94223f9722364a74
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2025-10-25 15:16:22 +0200

    Add support for swww to wallpaper command

--- scripts/wallpaper_command.sh
+++ scripts/wallpaper_command.sh
@@ -1,11 +1,44 @@
 #! /bin/bash
+if  [ "$XDG_SESSION_TYPE" = wayland ]; then
+    # damn compatibility obsession
+    feh2swww() {
+        local q
+        q="$(swww query)"
+        paste <(tr<<<"$q" '\n' '\0'|sed -z 's/[^:]*: \([^:]*\).*/\n\1/')\
+              <(tr<"$HOME/.fehbg" '\n' '\0'|sed -zn "/^#/d;s/[^']*'//;s/' '/\\x00/g;s/'\\\\''/'/g;s/'\\s*$//
+                        $(sed 'cp;'<<<"$q"|tr -d '\n')" |\
+                sed -z 's/\n$//')\
+            -z -d: |\
+            sed -nz '/^[^\n]/q;s/^\n//;s/^\([^:]*\):/-o\x00\1\x00/p' |\
+            xargs -0n3 swww img -t wave
+    }
+    setwallpaper() {
+        readarray -d $'\x00' files
+        files=( "${files[@]//"'"/"'\\''"}" )
+        files=( "${files[@]/#/"'"}" )
+        files=( "${files[@]/%/"'"}" )
+        cat > "$HOME/.fehbg" << EOF
+#!/bin/sh
+feh --no-fehbg --bg-scale ${files[*]}
+EOF
+        feh2swww
+    }
+else
+    setwallpaper() {
+        xargs -0 feh --bg-scale
+    }
+fi
 if [ -z "${1+O}" ]; then
     # no arg
     if grep -q "00[^']*'"'\|^# locked' "$HOME/.fehbg"; then
-        "$HOME/.fehbg"
+        if $XORG; then
+            "$HOME/.fehbg"
+        else
+            feh2swww
+        fi
     else
         find "$HOME/Documents/.wallpaper" \( -type f -o -xtype f \) -print0 | \
-            grep -zv '\(^\|/\)00' | sort -zR | xargs -0 feh --bg-scale
+            grep -zv '\(^\|/\)00' | sort -zR | setwallpaper
     fi
 else
     LOCK=false
@@ -20,7 +53,7 @@ else
         '')
             # empty arg
             find "$HOME/Documents/.wallpaper" \( -type f -o -xtype f \) -print0 | \
-                grep -zv '\(^\|/\)00' | sort -zR | xargs -0 feh --bg-scale
+                grep -zv '\(^\|/\)00' | sort -zR | setwallpaper
             ;;
         *!)
             LOCK=true
@@ -28,7 +61,7 @@ else
             ;&
         *)
             find "$HOME/Documents/.wallpaper" \( -type f -o -xtype f \) -print0 | \
-                grep -zE "($1)" | sort -zR  | xargs -0 feh --bg-scale
+                grep -zE "($1)" | sort -zR  | setwallpaper
             ;;
     esac
     "$LOCK" && echo '# locked' >> "$HOME/.fehbg"
