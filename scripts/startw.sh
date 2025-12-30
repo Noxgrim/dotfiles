ede004fd35b62b202091ebdfde433ad535a9249e
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2025-12-30 20:44:04 +0100

    Set XDG_SESSION_TYPE explicitly

--- scripts/startw.sh
+++ scripts/startw.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 # shellcheck disable=2155
 export GTK_THEME=Arc:dark GTK2_RC_FILES=/usr/share/themes/Arc-dark/gtk-2.0/gtkrc QT_STYLE_OVERRIDE=Adwaita-Dark
 # set GNOME here so that Electron apps pick up gnome-keyring
-export XDG_CURRENT_DESKTOP=SWAY:GNOME
+export XDG_CURRENT_DESKTOP=SWAY:GNOME XDG_SESSION_TYPE=wayland
 
 eval "$(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets)"
 eval "$(ssh-agent -s)"
