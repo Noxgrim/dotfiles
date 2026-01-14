51b978d3cf875194be0a29a6cfc13703f875192c
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2026-01-14 22:38:12 +0100

    Use /sys/class/power_supply in battery check script
    This should avoid docks and similar to show up as a “battery” in acpi -b
    and causing the device to hibernate randomly (the order of the entries
    listed does not seem to be stable)

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-source /root/notify.sh
-acpi -b | sed 's/[^:]*:\s*\([^,]*\),\s*\([^%]*\)%.*/\1,\2/' | {
-    IFS=, read -r status capacity
+readonly DEV='BAT0'
+capacity="$(<"/sys/class/power_supply/$DEV/capacity")"
+status="$(<"/sys/class/power_supply/$DEV/status")"
 
-    if [ "$status" = Discharging ]; then
-        if [ "$capacity" -lt 5 ]; then
-            logger "Critical battery threshold"
-            notify -k batterywarn
-            notify -u critical "Critical battery threshold" "Hibernating..."
-            sleep 5
-            systemctl hibernate -i
-        elif [ "$capacity" -le 20 ]; then
-            notify -u critical -a 'noxgrim:power' -h "int:value:$capacity" "Low battery level: " % -n batterywarn
-        fi
-    elif [ -e "/tmp/$USER/notifications/batterywarn" ]; then
+if [ "$status" = Discharging ]; then
+    if [ "$capacity" -lt 5 ]; then
+        source /root/notify.sh
+        logger "Critical battery threshold"
         notify -k batterywarn
+        notify -u critical "Critical battery threshold" "Hibernating..."
+        sleep 5
+        systemctl hibernate -i
+    elif [ "$capacity" -le 20 ]; then
+        source /root/notify.sh
+        notify -u critical -a 'noxgrim:power' -h "int:value:$capacity" "Low battery level: " % -n batterywarn
     fi
-}
+elif [ -e "/tmp/$USER/notifications/batterywarn" ]; then
+    source /root/notify.sh
+    notify -k batterywarn
+fi
