ebd9e9b7174bc1bf8b9abd0f7528c5a4af4b76c6
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2026-06-20 22:23:29 +0200

    Fix wttr tooltip

--- config/waybar/wttr.in.sh
+++ config/waybar/wttr.in.sh
@@ -10,8 +10,8 @@ for _ in {1..5}; do
         text="$(sed 's/\s+/ /g' <<< "$text")"
         tooltip="$(curl -s "https://wttr.in/$GEO_LON,$GEO_LAT?Tqp0")"
         if [ "$?" == 0 ] && [ -n "$tooltip" ]; then
-            tooltip=$(head -n-1 <<< "$tooltip" | tail -n+4 | sed 's/^     //' | terminal-to-html)
-            dt="$(head -n1 <<< "$tooltip" | grep -o '^\s*')$(date '+(%R)')"
+            tooltip=$(tail -n+3 <<< "$tooltip" | sed 's/^    //' | terminal-to-html)
+            dt="$(head -n1 <<< "$tooltip" | grep -o '^\W*' | sed 's/./ /g')$(date '+(%R)')"
             jq -nc \
               --arg txt "$text" \
               --arg tlt "<tt>$tooltip"$'\n'"$dt</tt>" \
